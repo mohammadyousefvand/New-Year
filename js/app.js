42d5a5a2cfabbf9f7c1a2fb19c355fe129968067
pc <www.mmhmdmm83@gmail.com>
2022-02-22 17:44:25 +0330

    add image for new Year 1401

--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
         .start()
 
         .ifEnded(() => {
-            console.log('The countdown has ended!');
+            let year_title = document.querySelector('.year-title')
+            document.body.style.backgroundImage = 'url(images/spring.jpg)'
+            year_title.innerHTML = 'عید شما مبارک 😃🎉'
+            window.onresize = () => {
+                let bodyWidth = getComputedStyle(document.body)
+                let widthValue = parseInt(bodyWidth.width)
+
+                if (widthValue < 500) {
+                    document.body.style.backgroundImage = 'url(images/spring-mobile.jpg)'
+                } else {
+                    document.body.style.backgroundImage = 'url(images/spring.jpg)'
+                }
+            }
         });
 });
